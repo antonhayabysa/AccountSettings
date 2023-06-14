bad12c4e3c58676874bf53058bcb674f5cbfa831
Anton <mail.anton88@gmail.com>
2023-06-14 11:25:46 +0300

    add new file for app

--- src/components/InfoForm/InfoForm.js
+++ src/components/InfoForm/InfoForm.js
@@ -1,13 +1,19 @@
 import React from "react";
 import CardsInLeftContent from "./CardsInLeftContent/CardsInLeftContent";
-import RightContentForm from "./RightContentForm/RightContentForm";
+import arrowLeft from "../../img/arrowLeft.png";
+import dots from "../../img/dots.png";
+import Header from "./Header/Header";
+import Form from "./Form/Form";
+import styles from "./styles/index.css";
 
 const InfoForm = () => {
   return (
-    <div>
-      InfoForm
-      <CardsInLeftContent />
-      <RightContentForm />
+    <div className="wrapper">
+      <Header />
+      <div className="container">
+        <Form />
+        <CardsInLeftContent />
+      </div>
     </div>
   );
 };
--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,18 +1,13 @@
-// UserProfile.js
 import React from "react";
 import SideBar from "../SideBar/SideBar";
 import InfoForm from "../InfoForm/InfoForm";
 import styles from "./styles/index.css";
+import circle from "../../img/circle.png";
 const UserProfile = () => {
   return (
-    <div className="container">
-      UserProfile
-      <div className="sidebar">
-        <SideBar />
-      </div>
-      <div className="infoForm">
-        <InfoForm />
-      </div>
+    <div className="containe">
+      <SideBar />
+      <InfoForm />
     </div>
   );
 };
